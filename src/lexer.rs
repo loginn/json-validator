84eae49615e6334592fac8520389f25a2958af72
vincent <vincent@swapcard.com>
2022-01-16 14:57:40 +0100

    comments

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,7 @@
 use std::collections::HashSet;
 
+// Macro to create a set
+// Shamelessly stolen from stack overflow
 macro_rules! set {
     ( $( $x:expr ),* ) => {  // Match zero or more comma delimited items
         {
@@ -12,28 +14,31 @@ macro_rules! set {
     };
 }
 
+// All the valid tokens types in JSON
 #[derive(PartialEq, Debug)]
 pub enum TokenType {
-    OPBR,
-    CLBR,
-    OPSQ,
-    CLSQ,
-    COMMA,
-    STRING,
-    NUMBER,
-    TRUE,
-    FALSE,
-    NULL,
-    COLON,
-    EOF,
-    NONE
+    OPBR,   // {
+    CLBR,   // }
+    OPSQ,   // [
+    CLSQ,   // ]
+    COMMA,  // ,
+    STRING, // "*"
+    NUMBER, // 1, 1.0, 1e+1, -1, ...
+    TRUE,   // true
+    FALSE,  // false
+    NULL,   // null
+    COLON,  // :
+    EOF,    // \0
+    NONE    // Special type used for a lack of token when initializing
 }
 
+// Token structure
 #[derive(Debug)]
 pub struct Token {
     pub ttype: TokenType,
 }
 
+// Lexer structure
 pub struct Lexer {
     input: String,
     pos: usize,
@@ -43,6 +48,7 @@ pub struct Lexer {
     unsafe_code_point: HashSet<char>,
 }
 
+// Impl block for our lexer
 impl Lexer {
     pub fn new(input: &String) -> Lexer {
         let mut lexer = Lexer {
@@ -61,15 +67,18 @@ impl Lexer {
         lexer.whitespace = set![' ', '\n', '\r', '\t'];
         // Hashset for escapable characters
         lexer.esc = set!['"', '\\', '/', 'b', 'f', 'n', 'r', 't', 'u'];
+        // Hashset for unsafe code points
         lexer.unsafe_code_point = set!['"', '\\'];
 
         return lexer
     }
 
+    // Getter for pos
     pub fn pos(&self) -> usize {
         return self.pos
     }
 
+    // A function to skip whitespace in the input
     fn skip_whitespace(&mut self) {
         while let Some(c) = self.input.chars().nth(self.pos) {
             if self.whitespace.contains(&c) {
@@ -97,6 +106,7 @@ impl Lexer {
         }
     }
 
+    // A function to handle a number token (1, -1, 1.0, -1.0, 1e1, 1e+1, 1e-1, ...)
     fn number(&mut self) {
         //minus
         if self.current_char.unwrap() == '-' {
@@ -124,13 +134,17 @@ impl Lexer {
         }
     }
 
+    // Look at the next character without consuming it
     fn peek(&self) -> Option<char> {
         return self.input.chars().nth(self.pos + 1);
     }
 
+    // Handle hex number for \u strings
     fn hex_num(&mut self) {
+        // \u strings must have the form \u HEX HEX HEX HEX meaning we can loop over 0..3
         for _ in 0..3 {
             match self.current_char {
+                // Check if the character is a digit in base 16
                 Some(c) if !c.is_digit(16) => {
                     panic!("Invalid hex value")
                 },
@@ -140,9 +154,10 @@ impl Lexer {
         }
     }
 
-    fn string(&mut self) -> String {
-        let mut result = String::new();
+    // A function to handle string tokens (i.e. "*+")
+    fn string(&mut self) {
         while let Some(c) = self.current_char {
+            // check if the next character is escaped and only allow those not in self.esc
             if c == '\\' {
                 match self.peek() {
                     None => panic!("Invalid end of string at pos {}", self.pos),
@@ -150,27 +165,31 @@ impl Lexer {
                         if !self.esc.contains(&p) {
                             panic!("Invalid escaped character : {} at pos {}", p, self.pos)
                         } else if p == 'u' {
+                            // if the next char is unicode advance twice to skip the '\' and 'u'
                             self.advance();
                             self.advance();
+                            // parse the hex num
                             self.hex_num();
                         }
                     }
                 }
             } else if self.unsafe_code_point.contains(&c) || c.is_control() {
-                return result;
+                // if c is not a safe character, return (used if we find the final " otherwise the json isnt valid)
+                return
             }
-            result.push(c);
+            // loop through characters
             self.advance();
         }
         panic!("Invalid end of string at pos {}", self.pos)
     }
 
+    // consume and return the next char
     fn advance(&mut self) {
         self.pos += 1;
         self.current_char = self.input.chars().nth(self.pos);
-        // println!("{:?}", self.current_char)
     }
 
+    // Check if the lexer's input matches a given string (true, false, null)
     fn string_check(&mut self, str_check: &str) {
         for i in 0..str_check.len() {
             match self.current_char {
@@ -182,18 +201,22 @@ impl Lexer {
         }
     }
 
+    // check that the next characters form the token 'true'
     fn ttrue(&mut self) {
         self.string_check("true")
     }
 
+    // check that the next characters form the token 'false'
     fn tfalse(&mut self) {
         self.string_check("false")
     }
 
+    // check that the next characters form the token 'null'
     fn tnull(&mut self) {
         self.string_check("null")
     }
 
+    // get the next token based on the current character
     pub fn get_next_token(&mut self) -> Token {
         while let Some(c) = self.current_char {
             if self.whitespace.contains(&c) {
@@ -242,6 +265,7 @@ impl Lexer {
                 panic!("Invalid character {:?} at position {}", self.current_char, self.pos)
             }
         }
+        // return end of file token
         return Token {
             ttype: TokenType::EOF
         }
